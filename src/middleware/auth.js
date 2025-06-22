0bc2739a8ce444d92681d9ca2d95c45a74c28f59
LeonardoSaraceli <leonardolodi09@gmail.com>
2025-06-22 17:49:37 +0100

    Feat: add crypto module

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
 import { UnauthorizedError } from '../errors/ApiError.js'
 
 const verifyToken = (req, res, next) => {
   try {
     const headers = req.headers['authorization']
+    if (!headers) throw new UnauthorizedError('Authorization header missing')
 
     const token = headers.split(' ')[1]
+    if (!token) throw new UnauthorizedError('Token missing')
 
     const payload = jwt.verify(token, process.env.SECRET_KEY)
-
     req.user = payload
-
     next()
   } catch (error) {
     throw new UnauthorizedError('Invalid token, lacking credentials.')
@@ -18,18 +19,33 @@ const verifyToken = (req, res, next) => {
 }
 
 const validateShopifyWebhook = (req, res, next) => {
-  const hmac = req.headers['x-shopify-hmac-sha256']
-  const secret = process.env.SHOPIFY_WEBHOOK_SECRET
+  try {
+    const hmac = req.headers['x-shopify-hmac-sha256']
+    const secret = process.env.SHOPIFY_WEBHOOK_SECRET
 
-  const generatedHmac = crypto
-    .createHmac('sha256', secret)
-    .update(JSON.stringify(req.body))
-    .digest('base64')
+    if (!secret) {
+      return res.status(500).send('Server error')
+    }
 
-  if (crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(generatedHmac))) {
-    next()
-  } else {
-    throw new UnauthorizedError('Unauthorized.')
+    if (!hmac) {
+      throw new UnauthorizedError('Unauthorized')
+    }
+
+    const generatedHmac = crypto
+      .createHmac('sha256', secret)
+      .update(JSON.stringify(req.body))
+      .digest('base64')
+
+    const hmacBuffer = Buffer.from(hmac, 'base64')
+    const generatedHmacBuffer = Buffer.from(generatedHmac, 'base64')
+
+    if (crypto.timingSafeEqual(hmacBuffer, generatedHmacBuffer)) {
+      next()
+    } else {
+      throw new UnauthorizedError('Unauthorized')
+    }
+  } catch (error) {
+    throw new UnauthorizedError('Unauthorized')
   }
 }
 
